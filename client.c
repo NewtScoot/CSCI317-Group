39306d29f76f90dd1f0c42ef52185857df876516
Patrick Ayres <briceayres@gmail.com>
2016-04-28 19:18:46 -0400

    unignored the c files

---
+++ client.c
@@ -0,0 +1,256 @@
+//
+//  client.c
+//  Description: Sends request to server to subscribe to multicaster
+//
+//  Created by Patrick Ayres on 3/13/16.
+//  Copyright © 2016 Patrick Ayres. All rights reserved.
+//
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
+
+#define SERVER_PORT 5432
+#define MAX_LINE 256
+#define MAXNAME 100
+
+// define type numbers
+#define REG 121
+#define CONF 221
+#define ADD_REG 131
+#define RESP 231
+#define LEAVE 321
+#define CHAT_MESSAGE 421
+
+pthread_t thread;
+/* structure of the packet */
+struct packet{
+    short type;
+    int sockId;
+    int groupNum;
+    char data[MAXNAME];
+};
+struct packet packet_reg; // packet for sending
+struct packet packet_rec; // packet for receiving
+struct packet packet_multicast; // packet for getting multicast
+
+void waitFor (unsigned int secs) {
+    long retTime = time(0) + secs;     // Get finishing time.
+    while (time(0) < retTime);    // Loop until it arrives.
+}
+
+// join handler thread
+void *recv_thread(int *client_sockId)
+{
+    printf("client_sockId: %d\n", *client_sockId);
+    while(1){
+        
+        //Receive a reply from the server
+        if(recv(*client_sockId,&packet_multicast,sizeof(packet_multicast),0) < 0)
+        {
+            printf("\n Could not receive message from mulicaster \n");
+            exit(1);
+        }
+        
+        // check if type is 231
+        if(htons(packet_multicast.type) == RESP){
+            
+            // print received message
+            printf("RECEIVED:\n");
+            //printf("\tTYPE: %d\n", htons(packet_multicast.type));
+            //printf("\tSOCK_ID: %d\n", packet_multicast.sockId);
+            printf("\tChat Message: %s\n", packet_multicast.data);
+        }
+        packet_multicast.type = 0;
+    }
+    
+    return NULL;
+}
+
+int main(int argc, char* argv[])
+{
+    // declare variables
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    char *host;
+    char buf[MAX_LINE];
+    char serverReply[MAX_LINE];
+    int s, leave_s;
+    int len;
+    char *webAddress;
+    int groupNumber;
+    int sockId;
+    
+    // throw error if not enough arguments
+    if(argc >= 2){
+        host = argv[1];
+    }
+    else{
+        fprintf(stderr, "usage:newclient server\n");
+        exit(1);
+    }
+    
+    // grab the web address which needs to be converted
+    webAddress = argv[2];
+    
+    // group number of chat room to join
+    groupNumber = atoi(argv[3]);
+    
+    /* translate host name into peer's IP address */
+    hp = gethostbyname(host);
+    if(!hp){
+        fprintf(stderr, "unkown host: %s\n", host);
+        exit(1);
+    }
+    
+    /* active open */
+    if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+        perror("tcpclient: socket");
+        exit(1);
+    }
+    
+    /* build address data structure */
+    bzero((char*)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+    //sin.sin_port = htons(SERVER_PORT);
+    sin.sin_port = htons(atoi(argv[2]));
+    
+    if(connect(s,(struct sockaddr *)&sin, sizeof(sin)) < 0){
+        perror("tcpclient: connect");
+        close(s);
+        exit(1);
+    }
+    
+    // wait for one second
+    waitFor(1);
+    
+    /* Constructing the registration packet at client */
+    packet_reg.type = htons(REG);
+    strcpy(packet_reg.data, hp->h_name);
+    packet_reg.groupNum = htons(groupNumber);
+    
+    /* Send the registration packet to the server */
+    if(send(s,&packet_reg,sizeof(packet_reg),0) <0)
+    {
+        printf("\n Send failed\n");
+        exit(1);
+    }
+    
+    // print sent message
+    printf("SENT:\n");
+    printf("\tTYPE: %d\n", ntohs(packet_reg.type));
+    printf("\tGROUP_NUM: %d\n", ntohs(packet_reg.groupNum));
+    printf("\tDATA: %s\n", packet_reg.data);
+    
+    // wait for one second
+    waitFor(1);
+    
+    /* Constructing the registration packet at client */
+    packet_reg.type = htons(REG);
+    strcpy(packet_reg.data, hp->h_name);
+    packet_reg.groupNum = htons(groupNumber);
+    
+    /* Send the registration packet to the server */
+    if(send(s,&packet_reg,sizeof(packet_reg),0) <0)
+    {
+        printf("\n Send failed\n");
+        exit(1);
+    }
+    
+    // print sent message
+    printf("SENT:\n");
+    printf("\tTYPE: %d\n", ntohs(packet_reg.type));
+    printf("\tGROUP_NUM: %d\n", ntohs(packet_reg.groupNum));
+    printf("\tDATA: %s\n", packet_reg.data);
+    
+    // wait for one second
+    waitFor(1);
+    
+    /* Constructing the registration packet at client */
+    packet_reg.type = htons(REG);
+    strcpy(packet_reg.data, hp->h_name);
+    packet_reg.groupNum = htons(groupNumber);
+    
+    /* Send the registration packet to the server */
+    if(send(s,&packet_reg,sizeof(packet_reg),0) <0)
+    {
+        printf("\n Send failed\n");
+        exit(1);
+    }
+    
+    // print sent message
+    printf("SENT:\n");
+    printf("\tTYPE: %d\n", ntohs(packet_reg.type));
+    printf("\tGROUP_NUM: %d\n", ntohs(packet_reg.groupNum));
+    printf("\tDATA: %s\n", packet_reg.data);
+    
+    // listen for response
+    while(1){
+        
+        //Receive a reply from the server
+        if(recv(s,&packet_rec,sizeof(packet_rec),0) < 0)
+        {
+            printf("\n Could not receive confirmation packet \n");
+            exit(1);
+        }
+        
+        // check if type is 221
+        if(htons(packet_rec.type) == CONF){
+            
+            // print received message
+            printf("RECEIVED:\n");
+            printf("\tTYPE: %d\n", htons(packet_rec.type));
+            printf("\tSOCK_ID: %d\n", packet_rec.sockId);
+            printf("\tGROUP_NUM: %d\n", packet_rec.groupNum);
+            printf("\tDATA: %s\n", packet_rec.data);
+            
+            // save sockId
+            sockId = packet_rec.sockId;
+            pthread_create(&thread, NULL, recv_thread, &s);
+            //pthread_join(threads[0],&exit_value);
+            
+            // holds text for chat message
+            char chatMessage [100];
+
+            // loop getting input from user
+            while(1){
+                
+                // get input from the user for the chat room
+                printf("Enter a message: ");
+                scanf ( " %s", chatMessage);
+                strcpy(packet_reg.data, chatMessage);
+                packet_reg.type = htons(CHAT_MESSAGE);
+                
+                /* Send the chat message packet to the server */
+                if(send(s,&packet_reg,sizeof(packet_reg),0) <0)
+                {
+                    printf("\n Send failed\n");
+                    exit(1);
+                }
+                sleep(1);
+                // print sent message
+                printf("SENT:\n");
+                printf("\tTYPE: %d\n", ntohs(packet_reg.type));
+                printf("\tDATA: %s\n", packet_reg.data);
+            }
+        }
+//        // else if the message is from the multicaster
+//        else if(htons(packet_rec.type) == RESP){
+//            
+//            // print received message
+//            printf("MULTICAST MESSAGE RECEIVED:\n");
+//            printf("\tTYPE: %d\n", htons(packet_rec.type));
+//            printf("\tDATA: %s\n", packet_rec.data);
+//            
+//        }
+    }
+
+}
---
+++ server.c
@@ -0,0 +1,329 @@
+//
+//  server.c
+//  Description: Allows clients to subscribe to multicaster stream.
+//
+//  Created by Patrick Ayres on 3/13/16.
+//  Copyright © 2016 Patrick Ayres. All rights reserved.
+//
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+#include <arpa/inet.h> //inet_addr
+#include <unistd.h>    //write
+
+#define SERVER_PORT 5432
+#define MAX_LINE 256
+#define MAX_PENDING 5
+#define MAXNAME 100
+#define TABLE_SIZE 20
+
+// define type numbers
+#define REG 121
+#define CONF 221
+#define ADD_REG 131
+#define RESP 231
+#define LEAVE 321
+#define CHAT_MESSAGE 421
+
+/* structure of Registration Table */
+struct registrationTable{
+    int port;
+    char name[MAXNAME];
+    int req_no;
+};
+
+// will hold tables values
+struct registrationTable table[TABLE_SIZE];
+
+/* structure of the packet */
+struct packet{
+    short type;
+    int sockId;
+    int groupNum;
+    char data[MAXNAME];
+};
+
+// global table stuct
+typedef struct  {
+    int sockid;
+    int reqno;
+    int groupNum;
+}global_table;
+
+// multicast buffer
+typedef struct {
+    int isRead;
+    global_table packet;
+}circular_buffer;
+
+
+circular_buffer buffer[10];
+int bufferPointer = 0;
+global_table record[TABLE_SIZE];
+struct packet packet_rec;
+struct packet packet_res;
+struct packet packet_multicast;
+struct packet packet_chat[1000];
+pthread_t threads[3];   // need 3 threads in the server
+int *exit_value;
+int socketId = 0;
+int foundInRecordTable = 0;
+int foundInRecordTableByJoinHandler = 0;
+int recordTablePointer = 0;
+int tableHasValues = 0;
+
+// global mutex variable
+pthread_mutex_t my_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// waiting timer
+void waitFor (unsigned int secs) {
+    long retTime = time(0) + secs;     // Get finishing time.
+    while (time(0) < retTime);    // Loop until it arrives.
+}
+
+// multicaster thread
+void *multicaster()
+{
+    
+//    while(1){
+//        // multicast every 5 seconds
+//        waitFor(5);
+//        
+////        // mutex lock the global table
+        pthread_mutex_lock(&my_mutex);
+////        
+////        // check first record is global table to see
+////        //    if there is at least one client listed in the table
+////        if(tableHasValues == 1 ){
+////            
+////            // at least 1 client found
+////            // only run once
+////            // now only this once read the text file into memory
+////            if(packetContructed == 0){
+////                
+////                /* Constructing the multicast packet to send to clients */
+////                packet_multicast.type = htons(RESP);
+////
+////                // manually copy the message over since strcpy fails
+////                for(int i = 0; i < 100; i++){
+////                    packet_multicast.data[i] = text[i];
+////                }
+////                printf("\n");
+////                //strcpy(packet_multicast.data,text);  // this line fails so manually copied
+////                
+////                packetContructed++;
+////            }
+//        
+//            // send message to everyone in the table with request number > 3
+//            // see if client is already in the global record table
+//            for(int i = 0; i < TABLE_SIZE; i++){
+//                
+//                // if the record has at least 3 registration requests
+//                if(record[i].reqno >= 3){
+//                    
+//                    // attempt to send the packet
+//                    if(send(record[i].sockid,&packet_multicast,sizeof(packet_multicast),0) < 0)
+//                    {
+//                        printf("\n Send failed\n");
+//                        exit(1);
+//                    }
+//                    
+//                    // print sent message
+//                    printf("\nSENT MULTICAST MESSAGE TO SOCKET_ID: %d\n", record[i].sockid);
+//                    printf("\tTYPE: %d\n", ntohs(packet_multicast.type));
+//                    printf("\tDATA: %s\n", packet_multicast.data);
+//                }
+//            }
+//        }
+    
+        // mutex unlock the global table
+        pthread_mutex_unlock(&my_mutex);
+    
+    //}
+    
+    
+    /* the function must return something - NULL will do */
+    return NULL;
+    
+}
+
+// join handler thread
+void *join_handler(global_table *rec)
+{
+    int newsock;
+    //struct packet packet_reg;
+    newsock = rec->sockid;
+
+    // wait for additional requests
+    while(1){
+        packet_chat[newsock].type = 0;
+        if(recv(newsock,&packet_chat[newsock],sizeof(packet_chat[newsock]),0) < 0){
+            printf("\n Cound not receive second registration packet\n");
+            //exit(1);
+        }
+        
+        // log incoming message
+        if(ntohs(packet_chat[newsock].type) == REG && newsock == rec->sockid){
+            
+            // increment request number and log
+            rec->reqno++;
+            printf("INCOMING... RECORD FOUND: REQ_NO: %d  SOCK_ID: %d  GROUP_NUM: %d\n", rec->reqno, rec->sockid, rec->groupNum);
+            
+            
+            
+            // if the record has been found 3 times, exit
+            if(rec->reqno == 3){
+                
+                // mutex lock the global table
+                pthread_mutex_lock(&my_mutex);
+                
+                //enter data in the record table
+                record[recordTablePointer].reqno = rec->reqno;
+                record[recordTablePointer].sockid = rec->sockid;
+                record[recordTablePointer].groupNum = rec->groupNum;
+                recordTablePointer++;
+                tableHasValues = 1;
+                
+                // mutex unlock the global table
+                pthread_mutex_unlock(&my_mutex);
+                
+                // SEND ACK RESPONSE
+                char serverResponse[] = "Client Now Registered for Multicast Messages\n";
+            
+                /* Constructing the registration packet at client */
+                packet_res.type = htons(CONF);
+                strcpy(packet_res.data,serverResponse);
+                packet_res.sockId = rec->sockid;
+                packet_res.groupNum = rec->groupNum;
+        
+                /* Send the registration packet to the client */
+                if(send(newsock,&packet_res,sizeof(packet_res),0) <0)
+                {
+                    printf("\n Send failed\n");
+                    exit(1);
+                }
+                        
+                // print sent message
+                printf("SENT:\n");
+                printf("\tTYPE: %d\n", ntohs(packet_res.type));
+                printf("\tSOCK_ID: %d\n", packet_res.sockId);
+                printf("\tREGISTERED for GROUP: %d\n", packet_res.groupNum);
+                printf("\tDATA: %s\n", packet_res.data);
+                //break;
+
+            }
+            
+        }
+        else if(ntohs(packet_chat[newsock].type) == CHAT_MESSAGE && newsock == rec->sockid){
+            
+            // add message to buffer
+            printf("Incoming chat message from SOCK_ID: %d for GROUP_NUM: %d\n", newsock, ntohs(packet_chat[newsock].groupNum));
+        }
+    }
+    
+    printf("Join_Handler Closing...\n");
+    
+    // Leave the thread
+    pthread_exit(NULL);
+}
+
+int main(int argc, char* argv[])
+{
+    // declare variables
+    short incomingPort;
+    char *incomingAddress;
+    struct sockaddr_in sin;
+    struct sockaddr_in clientAddr;
+    int s, new_s;
+    int len;
+    global_table client_info, leave_info;
+    
+    // create multicaster thread
+    pthread_create(&threads[1], NULL, multicaster, NULL);
+
+    
+    /* setup passive open */
+    if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+        perror("tcpserver: socket");
+        exit(1);
+    }
+    
+    /* build address data structure */
+    bzero((char*)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(SERVER_PORT);
+    
+    // bind and listen for incoming
+    if(bind(s,(struct sockaddr *)&sin, sizeof(sin)) < 0){
+        perror("tcpclient: bind");
+        exit(1);
+    }
+    
+    printf("SERVER RUNNING on PORT %d...\n",ntohs(sin.sin_port));
+    listen(s, MAX_PENDING);
+    
+
+    while(1){
+        /* wait for connection, then receive and print text */
+        if((new_s = accept(s, (struct sockaddr *)&clientAddr, &len)) < 0){
+            perror("tcpserver: accept");
+            exit(1);
+        }
+        if(recv(new_s,&packet_rec, sizeof(packet_rec), 0) < 0) {
+            printf("\n Could not receive first registration packet \n");
+            exit(1);
+        }
+        
+        // log incoming message
+        if(ntohs(packet_rec.type) == REG){
+            
+            
+            // set found to false
+            foundInRecordTable = 0;
+            
+            // see if client is already in the global record table
+            for(int i = 0; i < TABLE_SIZE; i++){
+                
+                // check if socket ID is already there
+                if(record[i].sockid == new_s){
+                    foundInRecordTable = 1;
+                }
+            }
+            
+            // if the record was not found, record it and create join handler thread
+            if(foundInRecordTable == 0) {
+                
+                // print recieved new message
+                printf("\nRECEIVED NEW CLIENT:\n");
+                printf("\tTYPE: %d\n", ntohs(packet_rec.type));
+                printf("\tGROUP_NUM: %d\n", ntohs(packet_rec.groupNum));
+                printf("\tDATA: %s\n", packet_rec.data);
+                
+                // create the client info
+                client_info.reqno = 1;
+                client_info.sockid = new_s;
+                client_info.groupNum = ntohs(packet_rec.groupNum);
+                
+                printf("INCOMING... RECORD NOT FOUND: REQ_NO: %d  SOCK_ID: %d\n", client_info.reqno, client_info.sockid);
+                
+                
+                pthread_create(&threads[0], NULL, join_handler, &client_info);
+                //pthread_join(threads[0],&exit_value);
+                
+            }
+        }
+    }
+    
+}
+
